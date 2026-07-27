75b4d26385c8c04a2a33cf8094eabf2b49f359ea
unknown <rssk1401@gmail.com>
2026-07-27 22:32:43 +0530

    Ready for production

--- backend/Anomaly_Detection/urls.py
+++ backend/Anomaly_Detection/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework_simplejwt.views import ( TokenObtainPairView, TokenRefreshView, )
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     
@@ -37,4 +38,8 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-     urlpatterns += static( settings.MEDIA_URL, document_root=settings.MEDIA_ROOT )
+    urlpatterns += static( settings.MEDIA_URL, document_root=settings.MEDIA_ROOT, )
+else:
+    urlpatterns += [
+        re_path( r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}, )
+    ]
